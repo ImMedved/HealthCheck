23f7c83c3bc870651582bb5b7a236a93d385cfd0
Alexander_Kukharev <akukharev@setronica.com>
2024-03-31 18:06:12 +0200

    Minor changes

--- src/test/sql/fake_data.sql
+++ src/test/sql/fake_data.sql
@@ -6,4 +6,6 @@ CREATE TABLE server (
 );
 
 INSERT INTO server (name, address, error_counter) VALUES ('Server 1', 'http://fake-server-1.com', 0);
-INSERT INTO server (name, address, error_counter) VALUES ('Server 2', 'http://fake-server-2.com', 3);
+INSERT INTO server (name, address, error_counter) VALUES ('Server 2', 'http://fake-server-2.com', 1);
+INSERT INTO server (name, address, error_counter) VALUES ('Server 3', 'http://fake-server-3.com', 2);
+INSERT INTO server (name, address, error_counter) VALUES ('Server 4', 'http://fake-server-4.com', 3);
